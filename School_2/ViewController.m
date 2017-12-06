50e5023a8c03a53aa43b696c9271858ddd8fdeaa
Alexander Cherushnikov <SBT-Cherushnikov-AS@sbtatlas.sigma.sbrf.ru>
2017-12-06 20:19:10 +0300

    Новый код

--- School_2/ViewController.m
+++ School_2/ViewController.m
@@ -8,22 +8,25 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
+
+@property (nonatomic, strong) NSArray *troubleList;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	self.view.backgroundColor = [UIColor greenColor];
+	self.troubleList = @[@"trouble1", @"trouble2", @"trouble3"];
 }
 
-
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (IBAction)boom:(id)sender
+{
+	NSLog(@"BOOM !");
 }
-
 
 @end
