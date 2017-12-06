75f9af193bcb29d624e8e2ea40ad7af267321185
Дмитрий Жаров <zharov.1512@gmail.com>
2017-12-06 20:19:25 +0300

    Initial Commit

--- School_2/ViewController.m
+++ School_2/ViewController.m
@@ -9,21 +9,19 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) NSArray *troubleList;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor greenColor];
+    self.troubleList = @[@"trouble 1, " @"trouble2", @"trouble3"];
 }
 
-
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (IBAction)carambaAction:(id)sender {
+    
 }
-
 
 @end
