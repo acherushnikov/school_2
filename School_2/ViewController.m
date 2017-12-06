f75e2fa6680889b49e034cd97cbc969779137207
Pavel Pichkurov <p.a.pichkurov@gmail.com>
2017-12-06 20:23:02 +0300

    + Добавили кнопку + Реалтзовали ViewController

--- School_2/ViewController.m
+++ School_2/ViewController.m
@@ -10,20 +10,19 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSArray *troubleList;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor greenColor];
+    self.troubleList = @[@"trouble1",@"trouble2",@"trouble3"];
 }
-
-
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (IBAction)boom:(id)sender {
+    NSLog(@"boom");
 }
-
 
 @end
