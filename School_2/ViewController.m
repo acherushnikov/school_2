90af184caa56668d916ed2136d88221b89617acb
Razmik Khodzhoyan <razm89@yandex.ru>
2017-12-06 20:22:35 +0300

    Добавили кнопку преобзаовали файл имплементации

--- School_2/ViewController.m
+++ School_2/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (nonatomic, strong) NSArray *troubleList;
 
 @end
 
@@ -17,13 +18,19 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor greenColor];
+    self.troubleList = @[@"trouble1", @"trouble2", @"trouble3"];
 }
 
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
-}
+//- (void)didReceiveMemoryWarning {
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
 
+- (IBAction)carambaAction:(id)sender
+{
+    NSLog(@"BOOM !");
+}
 
 @end
